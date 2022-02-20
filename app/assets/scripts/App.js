a3b1b5f6663ac5dcdbc67cd7be3c3e4687fabaff
Sidnei <sidneim@hotmail.com>
2022-02-20 20:28:05 -0300

    Completed the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,16 +3,45 @@ import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll"
 import StickyHeader from "./modules/StickyHeader";
-
+/* import Modal from "./modules/Modal"; */
+/* cria uma nova instancia das classes acima */
+/* new Modal()  */
 let stickyHeader = new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60);
 let mobileMenu = new MobileMenu();
+let modal 
+
+//Carrendo um modulo JS on the fly 
+//listen for open click - somente carrega o modulo JS modal se usuario clickar no botão para evitar carregar na memoria quando o modulo for grande, so carrega quando o usuário clickar. 
+document.querySelectorAll(".open-modal").forEach(el => { el.addEventListener("click", e => {
+      e.preventDefault()
+      if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then((x) => {
+              /* cria uma nova instancia das classes como acima */
+              /* new Modal()  */
+              modal = new x.default()
+              setTimeout(() => modal.openTheModal(), 20)
+          }).catch(() => {
+              console.log("There was a problem.")
+          })
+      } else {
+            modal.openTheModal()  
+      }      
+})})
 
 
 if (module.hot) {
   module.hot.accept()
 }
+
+
+
+
+
+
+
+
 
 /* Exemplo de uso - ver arq. modules/Person.js somente uma deve estar ativada function Person  ou  class Person */
 /*
